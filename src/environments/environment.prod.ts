6ffdeb26d0027c4094fbb4ed55839d50d41312ee
JSGGY <alekseinavalforever@gmail.com>
2025-11-13 00:27:32 -0500

    gg

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// Archivo de environment para producción
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://yukgkgehbzyhuklwfdtb.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1a2drZ2VoYnp5aHVrbHdmZHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MTk0NjgsImV4cCI6MjA3ODM5NTQ2OH0.brMMapadVekJkHdh-6bTmcKZ8eerXSBpb3YjFIBphGw'
+  }
+};
+
